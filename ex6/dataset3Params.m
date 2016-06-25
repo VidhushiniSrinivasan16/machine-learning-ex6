67086175b59c360264a292c870877cc6df0b7d3d
Vidhushini.S <vidhushinisrinivasan@gmail.com>
2016-06-25 20:05:52 +0530

    SVM
    Spam Classifier

---
+++ ex6/dataset3Params.m
@@ -0,0 +1,42 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 0.01;
+sigma = 0.01;
+K=[0.01 0.03 0.1 0.3 1.0 3.0 10.0 30.0];
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+error=0.0;low_error=1.0;
+for i=1:8
+for j=1:8
+model=svmTrain(X, y,K(i), @(x1, x2) gaussianKernel(x1, x2, K(j)));
+predictions=svmPredict(model,Xval);
+error=mean(double(predictions~=yval));
+if(error<low_error)
+C=K(i);sigma=K(j);
+low_error=error;
+end
+
+disp(error);
+end
+end
+% =========================================================================
+disp("final:");
+disp(C);disp(sigma);
+end
